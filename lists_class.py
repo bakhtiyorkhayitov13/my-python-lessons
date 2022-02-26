471131c2113e05ca274fd9d19613342f73a67a04
Bakhtiyor <89449124+baxtiyor1308@users.noreply.github.com>
2022-02-26 12:57:16 +0500

    Create lists_class.py
    List metodlari bo'yicha misollar

---
+++ lists_class.py
@@ -0,0 +1,108 @@
+"""
+Created on Wed Feb 26 10:13:23 2022
+
+@author: Bakgtiyor Khayitov
+
+Theme: built-in methods on lists/arrays 
+(ro'yhatlar ustida bajariladigan metodlar)
+
+"""
+
+### append() method
+'''adds an element at the end of the list'''
+'''ro'yhat oxiriga element qo'shish'''
+                     
+# fruits = ['banana', 'kiwi', 'orange', 'apricot', 'cherry']
+# fruits.append('lemon')
+# print(fruits)
+
+
+### clear() method
+'''removes all the elemens from the list'''
+'''ro'yhatdan barcha elementlarni chiqarib tashlash'''
+
+# fruits = ['banana', 'kiwi', 'orange', 'apricot', 'cherry']
+# fruits.clear()
+# print(fruits)
+
+
+### copy() method
+'''returns a copy of the list'''
+'''ro'yhat nusxasini qaytarish'''
+
+# fruits = ['banana', 'kiwi', 'orange', 'apricot', 'cherry']
+# fruits.copy()
+# print(fruits)
+
+
+### count() method
+'''Returns the number of elements with the specified value'''
+'''Belgilangan qiymatga ega elementlar sonini qaytaradi'''
+
+# prices = [3600, 2500, 10000, 1999, 150, 2500, 2500]
+# print(prices.count(2500))
+
+
+### extend() method
+'''Add the elements of a list (or any iterable), to the end of the current list'''
+'''Joriy ro'yxatning oxiriga ro'yxat elementlarini (yoki har qanday takrorlanadigan) qo'shing'''
+
+# cars = ['audi', 'bmw', 'nexia 3', 'bugatti', 'toyota']
+# cars.extend("matiz")
+# print(cars)
+
+
+### index() method
+'''Returns the index of the first element with the specified value'''
+'''Belgilangan qiymatga ega birinchi elementning indeksini qaytaradi'''
+
+# cars = ['audi', 'bmw', 'nexia 3', 'bugatti', 'toyota']
+# print(cars.index('bmw'))
+
+
+### insert() method
+'''Adds an element at the specified position'''
+'''Belgilangan joyga element qo'shish'''
+
+# cars = ['audi', 'bmw', 'nexia 3', 'bugatti', 'toyota']
+# cars.insert(3, 'tico')
+# print(cars)
+
+
+### pop() method
+'''Removes the element at the specified position'''
+'''Belgilangan pozitsiya(index)dagi elementni ro'yxatdan olib tashlash'''
+
+# cars = ['audi', 'bmw', 'nexia 3', 'bugatti', 'toyota']
+# cars.pop(3)
+# print(cars)
+
+
+### remove() methods
+'''Removes the first item with the specified value'''
+'''Ro'yhatdan belgilangan elementni olib tashlash'''
+
+# cars = ['audi', 'bmw', 'nexia 3', 'bugatti', 'toyota']
+# cars.remove('nexia 3')
+# print(cars)
+
+
+### reverse() method
+'''Reverses the order of the list'''
+'''Ro'yxat tartibini o'zgartirish'''
+
+# cars = ['audi', 'bmw', 'nexia 3', 'bugatti', 'toyota']
+# cars.reverse()
+# print(cars)
+
+
+### sort() method
+'''Sorts the list'''
+'''Ro'yxatni alifbo tartibida taxlash'''
+
+# cars = ['audi', 'bmw', 'nexia 3', 'bugatti', 'toyota']
+# cars.sort()
+# print(cars)
+
+
+###bakhtiyor1308
